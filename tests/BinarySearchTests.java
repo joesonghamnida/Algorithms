442ba8bb6b4b8d521f8940a0183dd33962746f53
Joe Morris <joe@mpb.hsd1.tn.comcast.net>
2016-12-05 19:10:55 -0600

    Simple tests for binary search

--- tests/BinarySearchTests.java
+++ tests/BinarySearchTests.java
@@ -1,3 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class BinarySearchTests {
+
+    @Test
+    public void DoesBinarySearchFindItsTarget(){
+        ArrayList<Integer> numbers = GenerateNumbers.integersFullSet(0,100);
+        Assert.assertTrue((numbers.size() == 100));
+        Assert.assertTrue(BinarySearch.binarySearchTestableBoolean(24, numbers));
+        Assert.assertTrue(BinarySearch.binarySearchTestableBoolean(59, numbers));
+        Assert.assertTrue(BinarySearch.binarySearchTestableBoolean(0, numbers));
+    }
 }

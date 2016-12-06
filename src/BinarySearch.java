d51b4b949b11b8784079fd37a1f3f102e21b318a
Joe Morris <joe@mpb.hsd1.tn.comcast.net>
2016-12-05 19:10:28 -0600

    added directly testable binary search function

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -24,4 +24,30 @@ public class BinarySearch {
         System.out.printf("It took %d loops to find %d\n", count, target);
     }
 
+    public static Boolean binarySearchTestableBoolean(int target, ArrayList numbers){
+        Boolean found = true;
+        int min = 0;
+        int max = numbers.size() - 1;
+        int guess = (int) Math.floor((max + min) / 2);
+        int count = 0;
+
+        while(!found){
+
+            if(guess < target){
+                min =(int) guess + 1;
+            }
+            else if(guess > target){
+                max = (int) guess - 1;
+            }
+            else{
+                found = true;
+            }
+            guess = (int) Math.floor((max + min) / 2);
+            System.out.printf("Max: %d, Min: %d, Guess: %d, count: %d\n", max,min,guess,count);
+            count ++;
+        }
+
+        return found;
+    }
+
 }
